0a401c6a162cf81b280aa819df331f2f62e52464
eunkyunghyun <57312000+eunkyunghyun@users.noreply.github.com>
2022-04-08 00:41:20 +0900

    Update slang_detector.py

--- OpenCV/slang_detector.py
+++ OpenCV/slang_detector.py
@@ -1,4 +1,5 @@
 from cvzone.HandTrackingModule import HandDetector
+from playsound import playsound
 import cv2
 
 # Import the camera by a VideoCapture object
@@ -9,6 +10,7 @@ detector = HandDetector(detectionCon=0.8)
 colorR = (255, 0, 255)
 
 cx, cy, w, h = 100, 100, 200, 200
+count = 100
 
 while True:
     # Load an image taken consistently
@@ -24,8 +26,13 @@ while True:
         handType1 = hand1["type"]  # Indicate whether your hand is left or right
         fingers1 = detector.fingersUp(hand1)
         target = fingers1.pop(2)
-        if target == 1 and any(fingers1) == 0:
+        if count >= 100:
+            count = 10
+        if target == 1 and any(fingers1) == 0 and count >= 10:
             print("A slang expression is detected!")
+            playsound("./resource/caution.mp3")
+            count = 0
+        count += 1
     cv2.rectangle(frame, (cx - w // 2, cy - h // 2), (cx + w // 2, cy + h // 2), colorR, cv2.FILLED)
     cv2.imshow("Image", frame)
     cv2.waitKey(1)
